61bc64a093ce78e5bd8594cd83e55dbc1c8d1aa8
Marjolo <72655919+mlmabuti@users.noreply.github.com>
2022-11-19 19:24:49 +0800

    Create GoodPairs.js

---
+++ problems/GoodPairs.js
@@ -0,0 +1,11 @@
+var numIdenticalPairs = function(nums) {
+    let ctr = 0;
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (nums[i] == nums[j]) {
+                ctr++;
+            }
+        }
+    }
+    return ctr;
+};
